38f9de6ab0bea188df0e6a601d48956c5c946bf0
LackFos <80957646+LackFos@users.noreply.github.com>
2023-10-02 20:40:56 +0700

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='logo'>GOR KITA</div>
+      <div className='right'></div>
+    </header>
+  )
+}
+
+export default Header
