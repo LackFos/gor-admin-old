f9fb3b95f6d42ca998dfb6889a6ac34b3211490c
LackFos <80957646+LackFos@users.noreply.github.com>
2023-10-02 20:41:17 +0700

    Create aside component

---
+++ src/assets/icons/CalendarIcon.jsx
@@ -0,0 +1,87 @@
+const CalendarIcon = () => {
+  return (
+    <svg
+      className='icon'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M16.2383 3.5791H7.77096C4.83427 3.5791 3 5.21504 3 8.22213V17.2718C3 20.3261 4.83427 21.9999 7.77096 21.9999H16.229C19.175 21.9999 21 20.3545 21 17.3474V8.22213C21.0092 5.21504 19.1842 3.5791 16.2383 3.5791Z'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M3.09277 9.4043H20.9167'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M16.4424 13.3096H16.4516'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M12.0049 13.3096H12.0141'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M7.55762 13.3096H7.56688'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M16.4424 17.1963H16.4516'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M12.0049 17.1963H12.0141'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M7.55762 17.1963H7.56688'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M16.0439 2V5.29078'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M7.96582 2V5.29078'
+        stroke='#343434'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default CalendarIcon
